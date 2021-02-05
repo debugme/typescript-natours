40d01c0d4297063064f5119c1f73a30815ad00d9
Asad Razvi <debugme@hotmail.com>
2021-02-05 15:21:52 +0000

    add logic to connect to mongodb

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 • add instructions to start up a local mongodb instance in docker for development
 • add instructions to connect to mongodb instance in the cloud for production
+
+### USE LOCAL MONGODB
+
+## Download MongoDB
+
+```sh
+$ docker pull mongo
+```
+
+## Start up MongoDB
+
+```sh
+$ docker run -d -p 27017-27019:27017-27019 --name mongodb mongo
+```
+
+## Connect to MongoDB via Shell
+
+```sh
+$ docker exec -it mongodb mongo
+```
--- server.ts
+++ server.ts
@@ -1,15 +1,58 @@
 import express from 'express'
 import chalk from 'chalk'
+import dotenv from 'dotenv'
 import morgan from 'morgan'
+import mongoose from 'mongoose'
 
 import { toursRouter } from './routers/tours'
 import { usersRouter } from './routers/users'
 
+dotenv.config({
+  path:
+    process.env.NODE_ENV === 'production'
+      ? '.production.env'
+      : '.development.env',
+})
+
+const {
+  env: {
+    MONGO_PROTOCOL = '',
+    MONGO_USERNAME = '',
+    MONGO_PASSWORD = '',
+    MONGO_HOSTNAME = '',
+    MONGO_DATABASE = '',
+  },
+} = process
+
+const MONGO_CREDENTIALS =
+  MONGO_USERNAME && MONGO_PASSWORD ? `${MONGO_USERNAME}:${MONGO_PASSWORD}@` : ''
+
+const mongoUri = `${MONGO_PROTOCOL}${MONGO_CREDENTIALS}${MONGO_HOSTNAME}/${MONGO_DATABASE}?retryWrites=true&w=majority`
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+}
+
+mongoose
+  .connect(mongoUri, options)
+  .then((connection) => {
+    const { name, host, port, user, pass } = connection.connections[0]
+    console.info(
+      `connection: \nname:${name}\nhost:${host}\nport:${port}\nuser:${user}\npass:${pass}`
+    )
+  })
+  .catch((error) => console.error(error))
+
 const server = express()
+
 server.use(morgan('dev'))
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
 server.use(express.static(`${process.cwd()}/public`))
+
 server.use('/api/v1/tours', toursRouter)
 server.use('/api/v1/users', usersRouter)
 
