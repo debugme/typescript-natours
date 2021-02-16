d4dbe10715b3d59b7d336ea0ce65d8aeebdd3a30
Asad Razvi <debugme@hotmail.com>
2021-02-16 16:31:40 +0000

    deduplicate code

--- index.ts
+++ index.ts
@@ -4,7 +4,6 @@ import { buildTourRouter } from './routers/tourRouter'
 import { buildUserRouter } from './routers/userRouter'
 import { defaultRouter } from './controllers/defaultController'
 import { errorHandler } from './controllers/errorHandler'
-import { buildReviewRouter } from './routers/reviewRouter'
 import { Services } from './services'
 
 const services = new Services(process)
@@ -15,25 +14,45 @@ database.connect()
 const server = new Server(services)
 server.handleRequest('/api/v1/tours', buildTourRouter(services))
 server.handleRequest('/api/v1/users', buildUserRouter(services))
-server.handleRequest('/api/v1/reviews', buildReviewRouter(services))
 server.handleRequest('*', defaultRouter)
 server.handleError(errorHandler)
 server.connect()
 
-process.on('uncaughtException', async (error: Error) => {
+const cleanUp = async (error: Error) => {
   await database.disconnect()
   await server.disconnect()
   await new Promise((resolve) => {
     console.log('[process] terminating...', error)
     resolve(process.exit(1))
   })
-})
+}
+process.on('uncaughtException', cleanUp)
+process.on('unhandledRejection', cleanUp)
 
-process.on('unhandledRejection', async (error: Error) => {
-  await database.disconnect()
-  await server.disconnect()
-  await new Promise((resolve) => {
-    console.log('[process] terminating...', error)
-    resolve(process.exit(1))
-  })
-})
+// const runtime = new Runtime(process, database, server)
+// runtime.connect()
+
+// interface Disconnectable {
+//   disconnect: () => {}
+// }
+
+// class Runtime {
+//   constructor(
+//     private process: NodeJS.Process,
+//     ...disconnectables: Disconnectable[]
+//   ) {
+//     this.disconnectables = disconnectables
+//   }
+//   connect = () => {
+//     this.process.on('uncaughtException', this.cleanUp)
+//     this.process.on('unhandledRejection', this.cleanUp)
+//   }
+//   cleanUp = async (error: Error) => {
+//     await database.disconnect()
+//     await server.disconnect()
+//     await new Promise((resolve) => {
+//       console.log('[process] terminating...', error)
+//       resolve(this.process.exit(1))
+//     })
+//   }
+// }
